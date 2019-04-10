1491eb53e6b44af124d149e35dc59e0ecc5ccc31
Jen Nummerdor <jennifer.nummerdor@gmail.com>
2019-04-09 18:30:44 -0700

    buyer modal styling done

--- client/src/components/Buy.jsx
+++ client/src/components/Buy.jsx
@@ -4,10 +4,11 @@ const Buy = ({view}) => {
   return (<div>
     {view === 'Buy' ? (
     <div className="superNavBuyModal">
-      <h3 className="modalTitle">Phoenix homes for sale</h3>
-        {col1.map((col1Item, idx) => <span className="col1Item" key={idx}>{col1Item}</span>)}
-      <h3 className="modalTitle">Resources</h3>
-        {col2.map((col2Item, idx) => <span className="col2Item" key={idx}>{col2Item}</span>)}
+      <h3 id="modalBuyTitle1">Phoenix homes for sale</h3>
+        {col1.map((col1Item, idx) => <span id={"buyModalColOne" + idx} key={idx}>{col1Item}</span>)}
+      <span id="buyDivider"></span>
+      <h3 id="modalBuyTitle2">Resources</h3>
+        {col2.map((col2Item, idx) => <span id={"buyModalColTwo" + idx} key={idx}>{col2Item}</span>)}
     </div>
     ) : (
     <div></div>
